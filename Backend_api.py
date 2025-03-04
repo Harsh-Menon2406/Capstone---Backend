ef13fa7862ec2c9ed64be5300e95e1d15bdc553a
Harsh-Menon2406 <164702531+Harsh-Menon2406@users.noreply.github.com>
2025-03-04 15:01:39 -0800

    Update Backend_api.py

--- Backend_api.py
+++ Backend_api.py
@@ -87,5 +87,5 @@ def search_tags():
     return jsonify([dict(row) for row in rows])
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=50000, debug=True)
+    app.run(host="0.0.0.0", port=10000, debug=True)
 
