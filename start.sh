69740e15bb7213a10413ddbcd970c56b94f417b7
Harsh-Menon2406 <164702531+Harsh-Menon2406@users.noreply.github.com>
2025-03-03 22:00:09 -0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,6 @@
+#!/bin/bash
+if [ ! -f "/opt/render/project/src/project_database.db" ]; then
+    echo "Database file not found, creating..."
+    python /opt/render/project/src/database_setup.py
+fi
 gunicorn -w 4 -b 0.0.0.0:10000 Backend_api:app
